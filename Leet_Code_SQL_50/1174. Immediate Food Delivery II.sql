f17bdd094f55f8bebb8c1b0a4944791e145e5e08
SrinathBegudem <156487657+SrinathBegudem@users.noreply.github.com>
2024-08-24 00:32:00 -0700

    Update 1174. Immediate Food Delivery II.sql

--- Leet_Code_SQL_50/1174. Immediate Food Delivery II.sql
+++ Leet_Code_SQL_50/1174. Immediate Food Delivery II.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-Type 1
+--Solution 1 (beginner)
 select ROUND(AVG(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
 from Delivery d
 join
@@ -12,9 +12,12 @@ d.customer_id = f.customer_id
 and d.order_date = f.first_order_date
 
 
-Type 2
+--Solution 2 (Intermediate)
 select ROUND(AVG(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
 from Delivery d
 where (customer_id,order_date) in (select customer_id, min(order_date) as first_order_date
 from Delivery
 group by customer_id)
+
+--solution 3 (Advance)
+-- Using windows dense rank function (to be posted once i learn the concpets)
